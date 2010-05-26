d2c5b3e4d00530cdd3667f7b89d5e693a3544c49
James W <github@wogloms.net>
2010-05-26 18:14:17 -0400

    First commit!

---
+++ autofake.php
@@ -0,0 +1,12 @@
+<a href="javascript:;" onClick="autofake('http://fcps.edu/index.shtml','http://fcps.edu/favicon.ico','Fairfax County Public Schools');">FCPS</a> <br>
+<a href="javascript:;" onClick="autofake('http://fcps.blackboard.com/webapps/portal/frameset.jsp','http://blackboard.com/favicon.ico','Blackboard Academic Suite');">FCPS Blackboard</a> <br>
+<a href="javascript:;" onClick="autofake('http://mrnussbaum.com/','http://mrnussbaum.com/favicon.ico','www.mrnussbaum.com - A thousand sites in one; Math Games, educational games, science games, vocabulary games, social studies games and much more!');">MrNussbaum.com</a> <br>
+<a href="javascript:;" onClick="autofake('http://mrnussbaumpremium.com/','http://mrnussbaum.com/favicon.ico','MrNussbaum Premium Website Silver');">MrNussbaumPremium.com</a> <br>
+<a href="javascript:;" onClick="autofake('http://wogloms.net/','http://wogloms.net/favicon.ico','www.wogloms.net');">Wogloms.net</a> <br>
+<a href="javascript:;" onClick="autofake('http://google.com/','http://google.com/favicon.ico','Google');">Google</a> <br>
+<a href="javascript:;" onClick="autofake('http://glencoe.com/','http://glencoe.com/favicon.ico','Glencoe.com');">Glencoe</a> <br>
+<a href="javascript:;" onClick="autofake('about:tabs','','Welcome to Tabbed Browsing');">IE New Tab</a> <br>
+<a href="javascript:;" onClick="autofake('chrome://newtab','','New Tab');">Chrome New Tab</a> <br>
+<a href="javascript:;" onClick="autofake('chrome://newtab/','','The webpage cannot be displayed');">IE Page Cannot Be Displayed</a> <br>
+
+
